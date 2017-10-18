f7010e71460c714b22c9dfc250900f2e0ee5ebbe
Riki Montgomery and Sara Spink <sara@>
2017-10-18 15:06:00 -0700

    Get the API to append the manufacturer

--- js/bikefinder-interface.js
+++ js/bikefinder-interface.js
@@ -1,16 +1,33 @@
 let apiKey = require('./../.env').apiKey;
 
+// $(document).ready(function() {
+//   $('#bikeLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`).then(function(response) {
+//       $('.showManufacturer').text(`The Manufacturer in ${city} is ${response.bikes[0].manufacturer_name}`);
+//       debugger;
+//       $('.showModel').text(`The model is ${response.bikes.title}.`);
+//     }).fail(function(error) {
+//          $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//     });
+//   });
+// });
+
+
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    // let city = $('#location').val();
-    // $('#location').val("");
-    $.get(`bikeindex.org/api/v3/search?APPID=${apiKey}`).then(function(response) {
-      debugger;
-        $('.showBikeList').text(`${response.}`)
-        // $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        // $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
-      }).fail(function(error) {
-        $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+  $('#bikeLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`).then(function(response) {
+      response.bikes.forEach(function(bike){
+        $('.showManufacturer').append("</li>" + `${bike.manufacturer_name}` + "</li>");
+        // $('.showColor').text(`The color is ${bike.frame_colors}.`);
+
+      })
+
+    }).fail(function(error) {
+         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
 });
