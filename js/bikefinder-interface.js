6cf3f8370153365d48bac175a757206373fbe1a2
Riki Montgomery and Sara Spink <riki@>
2017-10-19 11:55:42 -0700

    try to add a pie chart

--- js/bikefinder-interface.js
+++ js/bikefinder-interface.js
@@ -1,27 +1,57 @@
 import { BikeFinder } from './../js/bikefinder.js';
 let apiKey = require('./../.env').apiKey;
+google.charts.load("current", {packages:["corechart"]});
 
 $(document).ready(function() {
   $('#search').click(function() {
 
     let city = $('#location').val();
-
     $('#location').val("");
 
     let finder = new BikeFinder(apiKey);
+
     try {
       finder.search(city, function(bikes) {
         bikes.forEach(function(bike){
+
           // $('#manufacturer').append("<li>" + `${bike.thumb}` + "</li>");
           $('#manufacturer').append("<li>" + `${bike.manufacturer_name}` + "</li>");
           $('#frame').append("<li>" + `${bike.frame_model}` + "</li>");
           $('#year').append("<li>" + `${bike.year}.` + "</li>");
           $('#color').append("<li>" + `${bike.frame_colors[0]}.` + "</li>");
-
         });
       });
     } catch(err) {
      $('.showErrors').text(`There was an error processing your request: ${err}. Please try again.`);
     }
+
+    // google bike color chart
+    finder.generateData(bike) {
+
+      let data = google.visualization.arrayToDataTable([
+        ['Bike Color', '# of Bikes'],
+        ['Black', bike.black_bikes],
+        ['Red', bike.red_bikes],
+        ['Yellow or Gold', bike.yellow_bikes],
+        ['Blue', bike.blue_bikes],
+        ['Green', bike.green_bikes],
+        ['Silver or Gray', bike.silver_bikes],
+        ['Orange', bike.orange_bikes],
+        ['White', bike.white_bikes],
+        ['Other', bike.other_bikes]
+      ]);
+
+      let options = {
+        // height: 400,
+        // width: 500,
+        title: 'Stolen bikes by color',
+        pieHole: 0.4,
+        colors:['#010102', '#E50000', '#FFFF32', '#3838CC', '#328432', '#666666', '#FFAE19', '#E5E5E5', '#4C004C']
+      };
+
+      let chart = new google.visualization.PieChart(document.getElementById('colorDonut')); 			chart.draw(data, options);
+
+      drawChart(data, options);
+    };
   });
 });
--- js/bikefinder.js
+++ js/bikefinder.js
@@ -2,7 +2,15 @@ export class BikeFinder {
   constructor(apiKey) {
     this.apiKey = apiKey;
     this.basePath = 'https://bikeindex.org/api/v3';
-
+    this.black_bikes = 0;
+    this.red_bikes = 0;
+    this.yellow_bikes = 0;
+    this.blue_bikes = 0;
+    this.green_bikes = 0;
+    this.silver_bikes = 0;
+    this.orange_bikes = 0;
+    this.white_bikes = 0;
+    this.other_bikes = 0;
   }
 // response.bikes may not work without calling getJSON
   search(city, result) {
@@ -14,4 +22,45 @@ export class BikeFinder {
       throw(error.responseText);
     });
   }
+
+  generateData(result) {
+    $.get(`${this.basePath}/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`)
+    .then(function(bike) {
+      result(response.bikes.forEach(bike) {
+        let bikeColor = bike.frame_colors[0];
+        switch(bike_color) {
+          case "Black":
+            this.black_bikes += 1;
+            break;
+          case "Red":
+            this.red_bikes += 1;
+            break;
+          case "Yellow or Gold":
+            this.yellow_bikes += 1;
+            break;
+          case "Blue":
+            this.blue_bikes += 1;
+            break;
+          case "Green":
+            this.green_bikes += 1;
+            break;
+          case "Silver or Gray":
+            this.silver_bikes += 1;
+            break;
+          case "Orange":
+            this.orange_bikes += 1;
+            break;
+          case "White":
+            this.white_bikes += 1;
+            break;
+          default:
+            this.other_bikes += 1;
+          }
+      });
+    })
+    .fail(function(error) {
+      throw(error.responseText);
+    });
+  }
+
 }
