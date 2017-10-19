e1355cd07b5ec0ce441267e751b62dfed9afc4a9
Riki Montgomery <mostriki820@gmail.com>
2017-10-18 20:00:20 -0700

    create a BikeFinder class and seperate our logic

--- js/bikefinder-interface.js
+++ js/bikefinder-interface.js
@@ -1,19 +1,5 @@
+import { BikeFinder } from './../js/bikefinder.js';
 let apiKey = require('./../.env').apiKey;
-
-// $(document).ready(function() {
-//   $('#bikeLocation').click(function() {
-//     let city = $('#location').val();
-//     $('#location').val("");
-//     $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`).then(function(response) {
-//       $('.showManufacturer').text(`The Manufacturer in ${city} is ${response.bikes[0].manufacturer_name}`);
-//       debugger;
-//       $('.showModel').text(`The model is ${response.bikes.title}.`);
-//     }).fail(function(error) {
-//          $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-//     });
-//   });
-// });
-
 
 $(document).ready(function() {
   $('#search').click(function() {
@@ -22,19 +8,20 @@ $(document).ready(function() {
 
     $('#location').val("");
 
-    $.get(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`).then(function(response) {
+    let finder = new BikeFinder(apiKey);
+    try {
+      finder.search(city, function(bikes) {
+        bikes.forEach(function(bike){
+          // $('#manufacturer').append("<li>" + `${bike.thumb}` + "</li>");
+          $('#manufacturer').append("<li>" + `${bike.manufacturer_name}` + "</li>");
+          $('#frame').append("<li>" + `${bike.frame_model}` + "</li>");
+          $('#year').append("<li>" + `${bike.year}.` + "</li>");
+          $('#color').append("<li>" + `${bike.frame_colors[0]}.` + "</li>");
 
-      response.bikes.forEach(function(bike){
-
-        // $('#manufacturer').append("<li>" + `${bike.thumb}` + "</li>");
-        $('#manufacturer').append("<li>" + `${bike.manufacturer_name}` + "</li>");
-        $('#frame').append("<li>" + `${bike.frame_model}` + "</li>");
-        $('#year').append("<li>" + `${bike.year}.` + "</li>");
-        $('#color').append("<li>" + `${bike.frame_colors[0]}.` + "</li>");
-
-      })
-    }).fail(function(error) {
-         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-    });
+        });
+      });
+    } catch(err) {
+     $('.showErrors').text(`There was an error processing your request: ${err}. Please try again.`);
+    }
   });
 });
---
+++ js/bikefinder.js
@@ -0,0 +1,17 @@
+export class BikeFinder {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.basePath = 'https://bikeindex.org/api/v3';
+
+  }
+// response.bikes may not work without calling getJSON
+  search(city, result) {
+    $.get(`${this.basePath}/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`)
+    .then(function(response) {
+      result(response.bikes);
+    })
+    .fail(function(error) {
+      throw(error.responseText);
+    });
+  }
+}
